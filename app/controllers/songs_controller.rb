7d257d22c4e018a3bbfaca979f4bb87eb2220ef4
Chris Cardle <cdcardle@gmail.com>
2018-04-16 12:12:23 +0000

    automatically backed up by learn

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,19 @@
+class SongsController < ApplicationController
+
+  get '/songs' do
+    erb :'/songs/index'
+  end
+
+  get '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'/songs/show'
+  end
+
+  get 'songs/new' do
+    erb :'/songs/new'
+  end
+
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+  end
+end
