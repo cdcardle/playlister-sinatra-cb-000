b73a872ceb660e159114ad5483909c8e42b7e49c
Chris Cardle <cdcardle@gmail.com>
2018-04-16 12:33:33 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -16,7 +16,10 @@ class SongsController < ApplicationController
   post '/songs' do
     song = Song.create(name: params[:Name])
     song.artist = Artist.find_or_create_by(name: params[:"Artist Name"])
-    song.genre_ids
+    song.genre_ids = params[:genres]
+    song.save
+
+    redirect to '/songs/<%= song.slug %>'
   end
 
   get '/songs/:slug/edit' do
